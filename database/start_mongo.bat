305cc5a1a4aaf88a0f5da13ce58940a7107124c6
chuqq <chuqingq@qq.com>
2020-12-21 14:47:44 +0800

    mongo启动脚本

--- database/start_mongo.bat
+++ database/start_mongo.bat
@@ -1,3 +1,3 @@
-# TODO sudo docker run -it /bin/bash mongodb xxxx
-mongod --dbpath dbpath 
+# manjaro中直接使用官方的版本：ubuntu 20.04 tgz版本
+./mongodb-linux-x86_64-ubuntu2004-4.4.2/bin/mongod --dbpath dbpath
 
