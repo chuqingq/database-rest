cb74c4fcda5a16e8c4968d728fb3e00b6eb96c4a
chuqq <chuqingq@qq.com>
2020-12-22 17:00:08 +0800

    test.sh

--- rest-go/test.sh
+++ rest-go/test.sh
@@ -1,5 +1,5 @@
 # create
-curl -X POST http://127.0.0.1:8080/mydb/mycol -d '[{"mykey":"myvalue1"},{"mykey":"myvalue2"}]'
+curl -X POST 'http://127.0.0.1:8080/mydb/mycol' -d '[{"mykey":"myvalue1"},{"mykey":"myvalue2"}]'
 
 # retrieve
 curl -X GET 'http://127.0.0.1:8080/mydb/mycol?filter={"mykey":"myvalue1"}'
