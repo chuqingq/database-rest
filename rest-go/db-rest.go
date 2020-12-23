bd1d0955cbe33110449a9d180a6170c17a9d5615
chuqq <chuqingq@qq.com>
2020-12-23 16:48:31 +0800

    服务端支持跨域

--- rest-go/db-rest.go
+++ rest-go/db-rest.go
@@ -66,6 +66,9 @@ type body struct {
 }
 
 func colHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")             //允许访问所有域
+    w.Header().Add("Access-Control-Allow-Headers", "Content-Type") //header的类型
+    w.Header().Set("content-type", "application/json")             //返回数据格式是json
 	if r.Method != http.MethodPost {
 		log.Printf("method not post")
 		return
